2ef5480eceef07baeb2ce598709c03a0ffed7f80
linding3r <thomasjuullindinger@yahoo.dk>
2023-08-22 10:57:55 +0200

    solved 2.Strings

---
+++ 00._Miscellaneous/01._Intro_Exercises/2.Strings.js
@@ -0,0 +1,64 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const resultOne = parseFloat(numberOne) + parseFloat(numberTwo)
+
+console.log(resultOne);
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const resultTwo = parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo);
+
+console.log(resultTwo.toFixed(2));
+
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+const average = (one+two+three)/3;
+
+console.log(average.toFixed(5));
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+
+// Get me the character "c"
+
+const charC = letters.indexOf('c');
+
+console.log(charC);
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+const factDone = fact.replace('j','J');
+
+console.log(factDone);
+// --------------------------------------
+
+
