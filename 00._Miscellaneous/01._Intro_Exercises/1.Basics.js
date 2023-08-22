9b5ca0527777874b29f6bdf7b8cdbbbd4c081c50
linding3r <thomasjuullindinger@yahoo.dk>
2023-08-22 10:45:45 +0200

    Added some different ways to solve ex 1+2

--- 00._Miscellaneous/01._Intro_Exercises/1.Basics.js
+++ 00._Miscellaneous/01._Intro_Exercises/1.Basics.js
@@ -9,6 +9,7 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 console.log(`My first name is ${firstName} and my last name is ${lastName}`)
+console.log("My first name is", firstName,"and my last name is", lastName)
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -16,12 +17,16 @@ console.log(`My first name is ${firstName} and my last name is ${lastName}`)
 const year = "2022";
 const number = 1;
 
-const newYear = parseInt(year) + number;
-
 // Add the year plus the number
 // The result should be 2023
 // You cannot touch line 1 or 2
 
+const newYear = parseInt(year) + number;
+const newYear2 = Number(year) + number;
+const newYear3 = +year + number;
+
 console.log(newYear);
+console.log(newYear2);
+console.log(newYear3);
 
 // --------------------------------------
