0611d2a26adad99b4f8c30c24bf6cde1e151cf90
linding3r <thomasjuullindinger@yahoo.dk>
2023-09-12 19:31:30 +0200

    First test with html

--- 05._Time_To_Deploy/app.js
+++ 05._Time_To_Deploy/app.js
@@ -1,10 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = 8080;
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 app.listen(PORT, () => {
     console.log('Server is running on PORT:', PORT);
-})
+});
 
-//module.exports(app);
+
