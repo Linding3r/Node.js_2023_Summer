9012dcc01a26fa50ffa9994903cb13bea04a8052
linding3r <thomasjuullindinger@yahoo.dk>
2023-09-27 17:28:46 +0200

    added extra text to node.js tab

--- 07._Mandatory_I/public/js/darkmode.js
+++ 07._Mandatory_I/public/js/darkmode.js
@@ -1,26 +1,27 @@
-const darkModeToggle = document.getElementById("dark-mode-toggle");
+const darkModeToggle = document.getElementById('dark-mode-toggle');
 const body = document.body;
-const highlightStylesheet = document.getElementById("highlight-stylesheet");
+const highlightStylesheet = document.getElementById('highlight-stylesheet');
 
 function setDarkMode(isDarkMode) {
-  body.classList.toggle("dark-mode", isDarkMode);
-  highlightStylesheet.href = isDarkMode
-    ? "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-dark.min.css"
-    : "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-light.min.css";
-  const listExample = document.getElementById("list-example");
-  listExample.setAttribute("data-bs-theme", isDarkMode ? "dark" : "light");
-
+    body.classList.toggle('dark-mode', isDarkMode);
+    highlightStylesheet.href = isDarkMode
+        ? 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-dark.min.css'
+        : 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-light.min.css';
+    if (document.getElementById('list-example')) {
+        const listExample = document.getElementById('list-example');
+        listExample.setAttribute('data-bs-theme', isDarkMode ? 'dark' : 'light');
+    }
 }
 
-const isDarkMode = localStorage.getItem("dark-mode") === "true";
+const isDarkMode = localStorage.getItem('dark-mode') === 'true';
 
 darkModeToggle.checked = isDarkMode;
 
 setDarkMode(isDarkMode);
 
-darkModeToggle.addEventListener("change", () => {
-  const isChecked = darkModeToggle.checked;
-  setDarkMode(isChecked);
+darkModeToggle.addEventListener('change', () => {
+    const isChecked = darkModeToggle.checked;
+    setDarkMode(isChecked);
 
-  localStorage.setItem("dark-mode", isChecked);
+    localStorage.setItem('dark-mode', isChecked);
 });
